b1056dbed210efd3a9de0eb6cb407cda4642874b
Gustavo Souza <g.cflsouza@gmail.com>
2021-01-30 22:58:15 -0300

    [username] updates
    - show products in array
    - products styled

--- client/pages/catalogo/[username].js
+++ client/pages/catalogo/[username].js
@@ -1,15 +1,10 @@
 import { useRouter } from 'next/router';
 
-
-
 import { Container, Button, ButtonGroup, Col, Row, DropdownButton, Dropdown  } from 'react-bootstrap';
 import Image from 'next/image'
 import Link from 'next/link'
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
 
 export default function Catalogo({ user }) {
   const router = useRouter();
@@ -19,10 +14,8 @@ export default function Catalogo({ user }) {
     return <p>Carregando...</p>;
   }
 
-  
-
-  return (<>
-
+  return (
+  <>
 
   <header className="header-profile">
 
@@ -81,109 +74,40 @@ export default function Catalogo({ user }) {
 
 <Container>
 <Row className="product-list">
-      <Col xs={6} md={4} xl={3}>
-        <div className="item">
-
-          <img src="https://catalogo-sda.vercel.app/produtos/002.jpg" />
-
-          <h3>Nome do produto</h3>
-          <p>Descrição do produto</p>
-          <p><b>R$79,90</b></p>
-
-          <a href="https://wa.me/1XXXXXXXXXX?text=Eu%20tenho%20interesse%20no% {products.title}">Botão de comprar</a>
-
-        </div>
-      </Col>
-
-      <Col xs={6} md={4} xl={3}>
-        <div className="item">
-
-        <img src="https://catalogo-sda.vercel.app/produtos/002.jpg" />
-
-          <h3>Nome do produto</h3>
-          <p>Descrição do produto</p>
-          <p><b>Valor do produto</b></p>
-
-          <a href="#">Botão de comprar</a>
-
-        </div>
-      </Col>
-
-      <Col xs={6} md={4} xl={3}>
-        <div className="item">
 
 
-        <img src="https://catalogo-sda.vercel.app/produtos/002.jpg" />
+{user.products.map(( products ) => (
 
-          <h3>Nome do produto</h3>
-          <p>Descrição do produto</p>
-          <p><b>Valor do produto</b></p>
+<Col xs={6} md={4} xl={3} key={products._id}>
+  <div className="item">
 
-          <a href="#">Botão de comprar</a>
+  {/* 
+  
+  PRECISA ADICIONAR O NOME DO PROVEDOR NO NEXT.CONFIG PRA IMAGEM APARECER
+  
+  
+  <Image
+        src={`${(products.photo)}`}
+        alt={products.title}
+        width={120}
+        height={120}
+      /> */}
 
-     
-        </div>
-      </Col>
+    <img src={`${(products.photo)}`} />
 
-      <Col xs={6} md={4} xl={3}>
-        <div className="item">
+    <p className="title-product">{products.title}</p>
+    {/* <p>{products.photo}</p> */}
+    <p className="price-product">R${products.price}</p>
 
-        <img src="https://catalogo-sda.vercel.app/produtos/002.jpg" />
+    <a className="btn-comprar" target="_blank" href={`https://wa.me/55${encodeURIComponent(user.phone)}?text=Olá!%20Eu%20gostaria%20de%20comprar%20o%20produto:%20*${(products.title)}*`}>Botão de comprar</a>
 
-          <h3>Nome do produto</h3>
-          <p>Descrição do produto</p>
-          <p><b>Valor do produto</b></p>
+  </div>
+</Col>
+))} 
 
-          <a href="#">Botão de comprar</a>
 
-    
-        </div>
-      </Col>
 
-      <Col xs={6} md={4} xl={3}>
-        <div className="item">
 
-        <img src="https://catalogo-sda.vercel.app/produtos/002.jpg" />
-
-          <h3>Nome do produto</h3>
-          <p>Descrição do produto</p>
-          <p><b>Valor do produto</b></p>
-
-          <a href="#">Botão de comprar</a>
-
-    
-        </div>
-      </Col>
-
-      <Col xs={6} md={4} xl={3}>
-        <div className="item">
-
-        <img src="https://catalogo-sda.vercel.app/produtos/002.jpg" />
-
-          <h3>Nome do produto</h3>
-          <p>Descrição do produto</p>
-          <p><b>Valor do produto</b></p>
-
-          <a href="#">Botão de comprar</a>
-
-    
-        </div>
-      </Col>
-
-      <Col xs={6} md={4} xl={3}>
-        <div className="item">
-
-        <img src="https://catalogo-sda.vercel.app/produtos/002.jpg" />
-
-          <h3>Nome do produto</h3>
-          <p>Descrição do produto</p>
-          <p><b>Valor do produto</b></p>
-
-          <a href="#">Botão de comprar</a>
-
-    
-        </div>
-      </Col>
     </Row>
 </Container>
       
@@ -195,7 +119,7 @@ export default function Catalogo({ user }) {
 
       <Container>
         <p>{user.address}</p>
-        <p><a href="#">Feito com ❤️ por Gustavo</a></p>
+        <p><a className="footer-credits" href="#">Feito com ❤️ por Gustavo</a></p>
       </Container>
 
     </footer>
@@ -204,12 +128,18 @@ export default function Catalogo({ user }) {
   </>)
 }
 
+
+
+
 export const getStaticPaths = async () => {
   const response = await fetch(`http://localhost:5000/api/users`);
   const data = await response.json();
 
   const paths = data.map(member => {
-    return { params: { username: member.username } }
+    return { params: { 
+      username: member.username,
+      products: []
+    } }
   });
 
   return {
@@ -220,6 +150,7 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async (context) => {
   const { username } = context.params;
+  // const { products } = context.params;
 
   const response = await fetch(`http://localhost:5000/api/users/profile/${username}`);
   const data = await response.json();
@@ -227,6 +158,7 @@ export const getStaticProps = async (context) => {
   return {
     props: {
       user: data,
+      products: []
     },
     revalidate: 10,
   }
